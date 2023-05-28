21b70ace6a30ea25e2972a766f42a63ba388a7cb
Josh Kabo <joshkong1@gmail.com>
2023-05-27 17:18:34 -0700

    fire and water drop are hooked up. Water doesn't instance

---
+++ GameAssets/Items/water_drop.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+var timer
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var despawn_timer
+
+func _ready():
+	despawn_timer = get_node("despawn_timer")
+	despawn_timer.start()
+	# timer = 
+
+func _physics_process(_delta):
+	# apply the valocity as supplied from the start
+	move_and_slide()
+
+
+func _on_despawn_timer_timeout():
+	# delete when timer runs out
+	queue_free()
--- GameAssets/Player/Gun.gd
+++ GameAssets/Player/Gun.gd
@@ -1,18 +1,33 @@
 extends Node2D
 
+var fire_point
+var water_drop_scene = preload("res://water_drop.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	fire_point = get_node("firepoint")
+
+func handle_fire():
+	print(water_drop_scene)
+	# var level_scene = get_tree().current_scene
+	# var fired_drop = water_drop_scene.instance()
+	# fired_drop.global_position = fire_point.global_position
+	# fired_drop.global_rotation = global_rotation
+	# fired_drop.velocity = Vector2(0, -1).rotated(global_rotation) * 1000
+	# level_scene.add_child(fired_drop)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    # Get the position of the mouse relative to the node
+func _process(_delta):
+	# Get the position of the mouse relative to the node
 	var mouse_pos = get_global_mouse_position() - global_position
-    
-    # Calculate the angle between the node and the mouse
+	
+	# Calculate the angle between the node and the mouse
 	var angle = atan2(mouse_pos.y, mouse_pos.x)
-    
-    # Set the node's global rotation to the calculated angle - global for not being messed up by car rotation
+	
+	# Set the node's global rotation to the calculated angle - global for not being messed up by car rotation
 	global_rotation = angle
+
+	if Input.is_action_just_pressed("fire"):
+		handle_fire()
