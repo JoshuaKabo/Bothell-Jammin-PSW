f318491e60716e4a762355a325398c4a89810884
Josh Kabo <joshkong1@gmail.com>
2023-05-28 20:28:35 -0700

    start on camera for transition

---
+++ GameAssets/Camera2D.gd
@@ -0,0 +1,19 @@
+extends Camera2D
+
+
+@export var initial_pos: Node2D
+@export var goal_pos: Node2D
+@export var initialzoom: Vector2
+
+var smoothing = 0.1
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# set_follow_target(target_node)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
